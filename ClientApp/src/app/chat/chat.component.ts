877a84f26263a8cb6a8cb5ab0d3b41640fac196e
Vanegasmk <vanegasmk@gmail.com>
2020-10-19 14:05:47 -0600

    Generate componet chat

---
+++ ClientApp/src/app/chat/chat.component.ts
@@ -0,0 +1,44 @@
+import { Component, Inject } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { HubConnection, HubConnectionBuilder} from "@aspnet/signalr"; // signalR Import
+import { PublicChat } from "./chat.interface";
+import { ClientRequest } from "http";
+
+@Component({
+  selector: "app-chat",
+  templateUrl: "./chat.component.html",
+  styleUrls: ["./chat.component.css"],
+})
+export class ChatComponent {
+
+  public showForm = false;
+  public message: string = "";
+  public messages: string[] = [];
+  public hubConnection : HubConnection;
+  public user : PublicChat;
+  
+  constructor() {
+    this.builConnection();
+  }
+
+  builConnection() {
+    this.hubConnection = new HubConnectionBuilder().withUrl("/bingoHub").build();
+
+    this.hubConnection.on("SendMessage", (msg) => {
+      this.messages.push(msg);
+    })
+
+    this.hubConnection.start()
+    .then(() => console.log("Bingo Hub Connection is start!"))
+    .catch(() => console.log("Bingo Hub Connection not start"));
+
+  }
+
+
+ 
+
+
+  sendMessage() {
+    this.hubConnection.invoke("Echo",this.message)
+  }
+}
---
+++ ClientApp/src/app/chat/chat.interface.ts
@@ -0,0 +1,4 @@
+export class PublicChat
+{
+    public user: string;
+}
