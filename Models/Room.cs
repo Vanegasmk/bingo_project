4c1437b8be0915c35f947a17900b00f54e8936c2
Vanegasmk <vanegasmk@gmail.com>
2020-10-23 14:43:02 -0600

    Model room created

---
+++ Models/Room.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bingo_project.Models
+{
+    public class Room
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+    }
+}
