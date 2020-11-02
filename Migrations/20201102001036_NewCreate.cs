744d6cc788ba51e6785456e4b5af5c04f04fe6ca
unknown <jodavasi@hotmail.com>
2020-11-01 18:28:43 -0600

    dda

---
+++ Migrations/20201102001036_NewCreate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace project_bingo.Migrations
+{
+    public partial class NewCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "cardboards",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    numbers = table.Column<int[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_cardboards", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cardboards");
+        }
+    }
+}
