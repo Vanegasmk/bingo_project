39db0835d946eddbae7a3358059dc84f6204e741
Vanegasmk <vanegasmk@gmail.com>
2020-11-02 21:18:21 -0600

    Add new funtions

--- Hubs/BingoHub.cs
+++ Hubs/BingoHub.cs
@@ -8,14 +8,21 @@ namespace bingo_project.Hubs
 {
     public class BingoHub : Hub
     {
+
         public async Task AddToGroup(string roomId)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
         }
-        
-        
-       
 
+        public Task SendCountToGroup(string groupname,int user)
+        {
+            return  Clients.Group(groupname).SendAsync("SendCount",user);
+        }
 
+        public  Task SendNumbersBingo(string groupname,int number)
+        {
+            return Clients.Group(groupname).SendAsync("SendNumbers",number);
+        }
     }
+
 }
